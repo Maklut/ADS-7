7796ccf9450b6032d7ea1c96598fa8b2d5f40022
Egor <149178595+Maklut@users.noreply.github.com>
2026-06-05 20:36:27 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,51 +1,78 @@
-// Copyright 2022 NNTU-CS
+// Copyright 2021 NNTU-CS
 #include <gtest/gtest.h>
 #include "train.h"
 
 TEST(lab8, test1) {
   Train train;
-  train.addCar(false);
-  train.addCar(false);
+  int count = 2;
+
+  while (count--) {
+    train.addCar(false);
+  }
+
   int len = train.getLength();
-  EXPECT_EQ(len, 2);
+  int op = train.getOpCount();
+
+  ASSERT_EQ(len, 2);
+  ASSERT_EQ(op, 4);
 }
 
 TEST(lab8, test2) {
   Train train;
-  for (int i = 0; i < 8; i++) {
+  int count = 8;
+
+  while (count--) {
     train.addCar(false);
   }
+
   int len = train.getLength();
-  EXPECT_EQ(len, 8);
+  int op = train.getOpCount();
+
+  ASSERT_EQ(len, 8);
+  ASSERT_EQ(op, 16);
 }
 
 TEST(lab8, test3) {
   Train train;
-  for (int i = 0; i < 1000; i++) {
+  int count = 1000;
+
+  while (count--) {
     train.addCar(false);
   }
+
   int len = train.getLength();
-  EXPECT_EQ(len, 1000);
+  int op = train.getOpCount();
+
+  ASSERT_EQ(len, 1000);
+  ASSERT_EQ(op, 2000);
 }
 
 TEST(lab8, test4) {
   Train train;
-  train.addCar(true);
-  train.addCar(false);
-  train.addCar(true);
-  train.addCar(false);
+  int count = 4;
+
+  while (count--) {
+    train.addCar(true);
+  }
+
   int len = train.getLength();
-  EXPECT_EQ(len, 4);
+  int op = train.getOpCount();
+
+  ASSERT_EQ(len, 4);
+  ASSERT_EQ(op, 20);
 }
 
 TEST(lab8, test5) {
   Train train;
-  train.addCar(true);
-  train.addCar(true);
-  train.addCar(false);
-  train.addCar(false);
-  train.addCar(true);
-  train.addCar(false);
+  int count = 6;
+
+  while (count--) {
+    train.addCar(true);
+  }
+
   int len = train.getLength();
-  EXPECT_EQ(len, 6);
+  int op = train.getOpCount();
+
+  ASSERT_EQ(len, 6);
+  ASSERT_EQ(op, 42);
 }
